8de778fa7903fa105026c0f8816dde966b2b2b91
Kurnosov <dryush97@gmail.com>
2020-10-25 12:00:34 +0300

    feat: completed brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+// module.exports = function check(str, bracketsConfig) {
+//   const openBrackets = [];
+//   const closeBrackets = {};
+//   for (let i of bracketsConfig) {
+//     let op = i[0].trim();
+//     let up = i[1].trim();
+//     openBrackets.push(op);
+//     closeBrackets[op] = up;
+//   } 
+//   let stack = [];
+//   for (let u of str) {
+//     if (openBrackets.indexOf(u) !== -1) {
+//       stack.push(u);
+//     } else {
+//       if (u == stack[stack.length - 1]) {
+
+//       }
+//     }
+//   }
+// }
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const closeBrackets = []
+  const closeBracketsMap = {};
+  for (let i of bracketsConfig) {
+    let op = i[0].trim();
+    let up = i[1].trim();
+    openBrackets.push(op);
+    closeBrackets.push(up);
+    closeBracketsMap[op] = up;
+  } 
+  let stack = [];
+  for (let u of str) {
+    if (closeBrackets.indexOf(u) !== -1) {
+      if (stack.length === 0 || u !== closeBracketsMap[stack[stack.length - 1]]) {
+        if (openBrackets.indexOf(u) !== -1) {
+          stack.push(u);
+        } else {
+          return false
+        }
+      } else {
+        stack.pop()
+      }
+    } else if (openBrackets.indexOf(u) !== -1) {
+      stack.push(u);
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0
 }
+
